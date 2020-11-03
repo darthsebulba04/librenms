4cb07e1407498c087b97fde73b80cedbce76318e
Chris <31055115+darthsebulba04@users.noreply.github.com>
2020-11-03 09:27:19 -0500

    Update server-stats.blade.php

--- resources/views/widgets/server-stats.blade.php
+++ resources/views/widgets/server-stats.blade.php
@@ -5,7 +5,7 @@
         data-value="{{ $cpu }}"
         data-max="100"
         data-symbol="%"
-        data-title="CPU Usage"
+        data-title="CPU"
     ></div>
 </div>
 
@@ -16,8 +16,9 @@
             class="guage-{{ $id }}"
             data-value="{{ $mem['used'] }}"
             data-max="{{ $mem['total'] }}"
-            data-label="Mbytes"
-            data-title="{{ $mem['mempool_descr'] }} Usage"
+            data-title="{{ $mem['mempool_descr'] }}"
+            data-label="{{ $mem['used'] }}{{ $mem['unit'] }}"
+            data-pointer=true
         ></div>
     </div>
 @endforeach
@@ -29,8 +30,54 @@
             class="guage-{{ $id }}"
             data-value="{{ $disk['used'] }}"
             data-max="{{ $disk['total'] }}"
-            data-label="Mbytes"
-            data-title="{{ $disk['storage_descr'] }} Usage"
+            data-title="{{ $disk['storage_descr'] }}"
+            data-label="{{ $disk['used'] }}{{ $disk['unit'] }}"
+            data-pointer=true
+        ></div>
+    </div>
+@endforeach
+
+@foreach($fans as $key => $fan)
+    <div class="col-sm-{{ $columns }}">
+        <div
+            id="fan-{{ $key }}-{{ $id }}"
+            class="guage-{{ $id }}"
+            data-value="{{ $fan['current'] }}"
+            data-min="{{ $fan['low'] }}"
+            data-max="{{ $fan['high'] }}"
+            data-title="{{ $fan['descr'] }}"
+            data-label="{{ $fan['unit'] }}"
+            data-pointer=true
+        ></div>
+    </div>
+@endforeach
+
+@foreach($temps as $key => $temp)
+    <div class="col-sm-{{ $columns }}">
+        <div
+            id="temp-{{ $key }}-{{ $id }}"
+            class="guage-{{ $id }}"
+            data-value="{{ $temp['current'] }}"
+            data-min="{{ $temp['low'] }}"
+            data-max="{{ $temp['high'] }}"
+            data-title="{{ $temp['descr'] }}"
+            data-label="{{ $temp['unit'] }}"
+            data-pointer=true
+        ></div>
+    </div>
+@endforeach
+
+@foreach($voltages as $key => $voltage)
+    <div class="col-sm-{{ $columns }}">
+        <div
+            id="voltage-{{ $key }}-{{ $id }}"
+            class="guage-{{ $id }}"
+            data-value="{{ $voltage['current'] }}"
+            data-min="{{ $voltage['low'] }}"
+            data-max="{{ $voltage['high'] }}"
+            data-title="{{ $voltage['descr'] }}"
+            data-label="{{ $voltage['unit'] }}"
+            data-pointer=true
         ></div>
     </div>
 @endforeach
