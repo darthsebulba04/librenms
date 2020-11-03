53ca285dccdd9f82323e2c64cc5975c5bd5abb9e
Chris <31055115+darthsebulba04@users.noreply.github.com>
2020-11-03 09:30:45 -0500

    Update server-stats.blade.php

--- resources/views/widgets/settings/server-stats.blade.php
+++ resources/views/widgets/settings/server-stats.blade.php
@@ -26,6 +26,75 @@
             <option value="12" @if($columnsize == 12) selected @endif>12</option>
         </select>
     </div>
+
+    <div class="form-group">
+        <label class="control-label">@lang('Graphs')</label> </br>
+        <table>
+          <tr>
+            <td>CPUs</td>
+            <td>
+              <input type="radio" name="show_cpus" id="show-cpus-yes-{{ $id }}" value="1" @if($show_cpus) checked @endif> Yes
+            </td>
+            <td>
+              <input type="radio" name="show_cpus" id="show-cpus-no-{{ $id }}" value="0" @if(!$show_cpus) checked @endif> No
+            </td>
+          </tr>
+          <tr>
+            <td>Mempools</td>
+            <td>
+              <input type="radio" name="show_mems" id="show-mems-yes-{{ $id }}" value="1" @if($show_mems) checked @endif> Yes
+            </td>
+            <td>
+              <input type="radio" name="show_mems" id="show-mems-no-{{ $id }}" value="0" @if(!$show_mems) checked @endif> No
+            </td>
+          </tr>
+          <tr>
+            <td>Disks</td>
+            <td>
+              <input type="radio" name="show_disks" id="show-disks-yes-{{ $id }}" value="1" @if($show_disks) checked @endif> Yes
+            </td>
+            <td>
+              <input type="radio" name="show_disks" id="show-disks-no-{{ $id }}" value="0" @if(!$show_disks) checked @endif> No
+            </td>
+          </tr>
+          <tr>
+            <td>Fans</td>
+            <td>
+              <input type="radio" name="show_fans" id="show-fans-yes-{{ $id }}" value="1" @if($show_fans) checked @endif> Yes
+            </td>
+            <td>
+              <input type="radio" name="show_fans" id="show-fans-no-{{ $id }}" value="0" @if(!$show_fans) checked @endif> No
+            </td>
+          </tr>
+          <tr>
+            <td>Temps</td>
+            <td>
+              <input type="radio" name="show_temps" id="show-temps-yes-{{ $id }}" value="1" @if($show_temps) checked @endif> Yes
+            </td>
+            <td>
+              <input type="radio" name="show_temps" id="show-temps-no-{{ $id }}" value="0" @if(!$show_temps) checked @endif> No
+            </td>
+          </tr>
+          <tr>
+            <td>Voltages</td>
+            <td>
+              <input type="radio" name="show_voltages" id="show-voltages-yes-{{ $id }}" value="1" @if($show_voltages) checked @endif> Yes
+            </td>
+            <td>
+              <input type="radio" name="show_voltages" id="show-voltages-no-{{ $id }}" value="0" @if(!$show_voltages) checked @endif> No
+            </td>
+          </tr>
+        </table>
+    </div>
+
+    <div class="form-group">
+        <label class="control-label">@lang('Filters')</label> </br>
+        Mempools<input type="text" class="form-control" name="filter_mems" id="filter-mems-{{ $id }}" placeholder="@lang('Memory filter')" value="{{ $filter_mems }}"> </br>
+        Disks<input type="text" class="form-control" name="filter_disks" id="filter-disks-{{ $id }}" placeholder="@lang('Disk filter')" value="{{ $filter_disks }}"> </br>
+        Fans<input type="text" class="form-control" name="filter_fans" id="filter-fans-{{ $id }}" placeholder="@lang('Fan filter')" value="{{ $filter_fans }}"> </br>
+        Temps<input type="text" class="form-control" name="filter_temps" id="filter-temps-{{ $id }}" placeholder="@lang('Temp filter')" value="{{ $filter_temps }}"> </br>
+        Voltages<input type="text" class="form-control" name="filter_voltages" id="filter-voltages-{{ $id }}" placeholder="@lang('Voltage filter')" value="{{ $filter_voltages }}"> </br>
+    </div>
 @endsection
 
 @section('javascript')
